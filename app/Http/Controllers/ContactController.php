4cc050e96cde71229e44fe98c90b557f67676077
Yvan Cochet <yvan.cochet@heig-vd.ch>
2023-11-16 20:11:44 +0100

    Pint refacto

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -14,6 +14,7 @@ class ContactController extends Controller
     {
         $contacts = Contact::all();
         $contactsCount = Contact::count();
+
         return view('contacts.index', compact('contacts', 'contactsCount'));
     }
 
